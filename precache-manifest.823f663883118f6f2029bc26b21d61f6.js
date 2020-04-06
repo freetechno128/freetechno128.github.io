5b84523e1c34b0879135cad445d682885f48eded
Philippe Talarico <p.talarico20@gmail.com>
2020-04-06 18:09:23 +0200

    First commit

---
+++ precache-manifest.823f663883118f6f2029bc26b21d61f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d894eff334fec1f701d918b2463f55c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e04b0b20385be7fb7051",
+    "url": "/static/css/main.db03f86e.chunk.css"
+  },
+  {
+    "revision": "13adf33620878c521f30",
+    "url": "/static/js/2.89313d95.chunk.js"
+  },
+  {
+    "revision": "e04b0b20385be7fb7051",
+    "url": "/static/js/main.93176abe.chunk.js"
+  },
+  {
+    "revision": "d4ecd74d6a65ec3e3ae1",
+    "url": "/static/js/runtime-main.ed3bbf0b.js"
+  }
+]);
